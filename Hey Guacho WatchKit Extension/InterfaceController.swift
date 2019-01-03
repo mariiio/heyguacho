d3a84d481f7617c98b3c238e4470d472b889f49d
Mario Saul <mario_sasitt@hotmail.com>
2019-01-03 10:33:04 -0300

    Initial Commit

---
+++ Hey Guacho WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Hey Guacho WatchKit Extension
+//
+//  Created by Mario Saul on 1/3/19.
+//  Copyright © 2019 Mario Saul. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Hey Guacho/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hey Guacho
+//
+//  Created by Mario Saul on 1/3/19.
+//  Copyright © 2019 Mario Saul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
